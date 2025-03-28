0e94582fc41fe3ba659cfc938b7feb1f371e4996
ronaldodeschain <lotushall@gmail.com>
2025-03-28 17:18:04 -0300

    fim do dia

--- aula1.py
+++ aula1.py
@@ -1,1 +1,6 @@
 # permite escrever um comentário 
+print (123, 45 , sep="-")
+#sep vai definir que tipo de separador vai aparecer no lugar do espaço padrão
+#o exemplo abaixo é um exemplo sem o sep
+print (123, 54)
+print ("novos testes")
