93d20511eec3a6bd277ed0f8b979be60ded00cb2
ronaldodeschain <lotushall@gmail.com>
2025-03-31 11:48:10 -0300

    aula 5 - variaveis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pythonudemy
 Cursos de Python complementares
 Começando as primeiras linhas de git e python
+#Aula 4 - Conversão de tipos
+#O aula1 está sendo atualizado com conteudos das aulas ao invés de criar novos arquivos
--- aula1.py
+++ aula1.py
@@ -4,3 +4,14 @@ print (123, 45 , sep="-")
 #o exemplo abaixo é um exemplo sem o sep
 print (123, 54)
 print ("novos testes")
+#conversão de tipos/typecasting/coerção
+print(int('1'),type(int('1')))
+print(int('1'),type('1'))
+#somando str com int via conversão
+print(int('1')+ 1)
+#forçando de float pra int
+print(int(1.7))
+
+#Aula 5 - Variaveis = atribuir valor a um nome
+nome_completo = 'Erasmo Carlos'
+print('o nome completo do cara é ' + nome_completo)
