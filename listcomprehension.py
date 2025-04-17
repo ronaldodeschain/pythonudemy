81e08a65cccc12af1ccaa67a9fe7824be3e7e715
ronaldodeschain <lotushall@gmail.com>
2025-04-17 16:07:56 -0300

    list comprehension e manipulando listas com mapeamento dentro de for

---
+++ listcomprehension.py
@@ -0,0 +1,30 @@
+#list comprehension em Python
+#list comprehension é uma forma rapida de criar listas
+#a partir de iteraveis
+
+#print(list(range(10)))
+'''
+lista = []
+for numero in range(10):
+    lista.append(numero)
+#print(lista)
+lista = [
+    numero * 2
+    for numero in range(10)]
+print(lista)
+'''
+#Mapeamento de dados em list comprehension
+produtos = [
+    {'nome':'p1','preco':20},
+    {'nome':'p2','preco':10},
+    {'nome':'p3','preco':30},
+]
+novos_produtos = [
+    #assim eu consigo alterar o preco 
+    # #tabelado de todos os produtos aumtomaticamente.
+   {**produto,'preco':produto['preco']*1.05}
+   if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+#print(novos_produtos)
+print(* novos_produtos, sep='\n')
