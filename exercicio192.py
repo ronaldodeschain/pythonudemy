fbed020db996f99f88e438bb9c630eff33687f23
ronaldodeschain <lotushall@gmail.com>
2025-04-28 16:10:10 -0300

    exercicio de listar

---
+++ exercicio192.py
@@ -0,0 +1,71 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+ # Música para codar =)
+ # Everybody wants to rule the world - Tears for fears
+ # todo = [] -> lista de tarefas
+ # todo = ['fazer café'] -> Adicionar fazer café
+ # todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+ # desfazer = ['fazer café',] -> Refazer ['caminhar']
+ # desfazer = [] -> Refazer ['caminhar', 'fazer café']
+ # refazer = todo ['fazer café']
+ # refazer = todo ['fazer café', 'caminhar']
+
+def listar(tarefas):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para listar')
+        print()
+        return 
+    
+    print('Tarefas: ')
+    for tarefa in tarefas:
+        print(f'\t{tarefa}')
+
+def desfazer(tarefas,tarefas_refazer):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para desfazer')
+        print()
+        return 
+    
+    tarefa = tarefas.pop()
+    tarefas_refazer.append(tarefa)
+
+def refazer(tarefas,tarefas_refazer):
+    print()
+    if not tarefas_refazer:
+        print('Nenhuma tarefa para refazer')
+        print()
+        return 
+    
+    tarefa = tarefas_refazer.pop()
+    tarefas.append(tarefa)
+
+
+def adicionar(tarefa,tarefas):
+    print()
+    tarefa = tarefa.strip()
+    if not tarefa:
+        print('Digite uma tarefa!')
+        return 
+    tarefas.append(tarefa)
+    
+
+tarefas = []
+tarefas_refazer = []
+
+while True:
+    print('Comandos: listar, desfazer, refazer')
+    tarefa = input('Digite uma tarefa ou comando: ')
+
+    if tarefa == 'listar':
+        listar(tarefas)
+        continue
+    elif tarefa == 'desfazer':
+        desfazer(tarefas,tarefas_refazer)
+        continue
+    elif tarefa == 'refazer':
+        refazer(tarefas,tarefas_refazer)
+        continue
+    else:
+        adicionar(tarefa,tarefas)
+        continue
