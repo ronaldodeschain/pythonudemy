344efad26ab83558fadc153952be40540e19ed2e
ronaldodeschain <lotushall@gmail.com>
2025-04-28 19:23:19 -0300

    Reorganizando arquivos do curso

---
+++ aula198.py
@@ -0,0 +1,6 @@
+# # class - Classes são moldes para criar novos objetos.
+# As classes geram novos objetos (instancias) que 
+# podem ter seus proprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados 
+# internos para realizar varias ações. 
+# Por convenção, usamos Pascalcase para nomes de classes.
