27909ea56565ae30cfb68ff808ab2697bbc04979
ronaldodeschain <lotushall@gmail.com>
2025-04-28 13:54:25 -0300

    fim da manipulacao de txt basico

--- aula116.py
+++ aula116.py
@@ -1,4 +1,4 @@
-
+import os
 
 caminho_arquivo = 'C:\\Users\\Usuario\\Documents\\python\\pasta\\'
 caminho_arquivo += 'aula116.txt'
@@ -9,27 +9,41 @@ caminho_arquivo += 'aula116.txt'
 # arquivo.close() 
 
 #com with open
-with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#      arquivo.write('Linha 1\n')
+#      arquivo.write('Linha 2\n')
+#      arquivo.writelines(
+#           ('Linha 3\n','Linha 4\n')
+#      )
+#      arquivo.seek(0, 0)
+#      print(arquivo.read())
+
+# print('#' * 10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#      print(arquivo.read())
+#      print('Lendo\n')
+#      arquivo.seek(0,0)
+#      print(arquivo.readline(), end='')
+#      print(arquivo.readline().strip())
+#      print(arquivo.readline().strip())
+
+#      print('READLINES')
+#      arquivo.seek(0,0)
+#      for linha in arquivo.readlines():
+#           print(linha.strip())
+
+#append mode escrever a partir do fim do arquivo
+#encoding garante o idioma e o uso dos caracteres especiais
+with open(caminho_arquivo, 'a+', encoding='utf8') as arquivo:
      arquivo.write('Linha 1\n')
      arquivo.write('Linha 2\n')
      arquivo.writelines(
           ('Linha 3\n','Linha 4\n')
      )
-     arquivo.seek(0, 0)
-     print(arquivo.read())
-
-print('#' * 10)
-
-with open(caminho_arquivo, 'r') as arquivo:
-     print(arquivo.read())
-     print('Lendo\n')
-     arquivo.seek(0,0)
-     print(arquivo.readline(), end='')
-     print(arquivo.readline().strip())
-     print(arquivo.readline().strip())
-
-     print('READLINES')
      arquivo.seek(0,0)
      for linha in arquivo.readlines():
           print(linha.strip())
-    
+
+#os.remove(caminho_arquivo)
+#os.rename(caminho_arquivo,'aula116-2.txt')
