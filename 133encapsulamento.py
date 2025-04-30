6332182d988d3c4160bf5a49b744b3ab1cf6a02a
ronaldodeschain <lotushall@gmail.com>
2025-04-30 11:46:01 -0300

    pre commit encapsulamento

---
+++ 133encapsulamento.py
@@ -0,0 +1,8 @@
+#Python não tem modificadores de acesso
+# _ (um underline) = protected
+#   nao deve ser usado fora da classe
+# __(dois underlines) = private
+# `name mangling` (desconfiguraçao de nomes)
+class Foo:
+    def __init__(self):
+        pass
